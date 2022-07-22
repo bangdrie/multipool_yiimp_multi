1bda8a9c73f4d0580ef7fd3ae9e1b513c21450e6
bangdrie <107682068+bangdrie@users.noreply.github.com>
2022-07-22 16:56:36 +0700

    Update db_multi.sh

--- db_multi.sh
+++ db_multi.sh
@@ -46,8 +46,28 @@ echo -e "$GREEN DB users and passwords can be found in $STORAGE_ROOT/yiimp/.my.c
 
 cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/sql
 # import sql dump
-sudo zcat 2019-11-10-yiimp.sql.gz | sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName}
+sudo zcat 2020-11-10-yaamp.sql.gz | sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName}
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-04-24-market_history.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-04-27-settings.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-05-11-coins.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-05-15-benchmarks.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-05-23-bookmarks.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-06-01-notifications.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-06-04-bench_chips.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2016-11-23-coins.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2017-02-05-benchmarks.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2017-03-31-earnings_index.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2020-06-03-blocks.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2017-05-accounts_case_swaptime.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2017-06-payouts_coinid_memo.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2017-09-notifications.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2017-10-bookmarks.sql
 sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2018-09-22-workers.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2017-11-segwit.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2018-01-stratums_ports.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2018-02-coins_getinfo.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2019-03-coins_thepool_life.sql
+sudo mysql -u root -p"${DBRootPassword}" ${YiiMPDBName} --force < 2020-06-03-blocks.sql
 sudo sed -i '/max_connections/c\max_connections         = 800' /etc/mysql/my.cnf
 sudo sed -i '/thread_cache_size/c\thread_cache_size       = 512' /etc/mysql/my.cnf
 sudo sed -i '/tmp_table_size/c\tmp_table_size          = 128M' /etc/mysql/my.cnf
